a0d3a562a1b314040b0fe7e2c1c71d0ee57e2228
thedkss <danimoblend@gmail.com>
2025-07-01 17:32:12 +0530

    Data Type Conversion

---
+++ conversion.js
@@ -0,0 +1,26 @@
+// Conversion and Operations
+// let score = "33";
+// let score = "33ab";
+// let score = null;
+// let score = undefined;
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// // console.log(valueInNumber); // NaN will be printed - which is not a number
+// console.log(valueInNumber); // 0
+
+// Boolean Conversion
+// let x = 1;
+// let x = 0;
+// let x = "abc";
+// let x = "0";
+// let x = "";
+// let y = Boolean(x);
+// console.log(y); // true
+
+// String Conversion
+let x = 1;
+let y = String(x);
+console.log(typeof y); // string
+console.log(y); // string
